619881ad7fbe3df9dee43836fe8df8c82a51c60e
ualon <ualon1954@gmail.com>
2026-06-27 15:54:15 +0300

    Initial commit

---
+++ scripts/bmi.js
@@ -0,0 +1,57 @@
+const age = document.getElementById('age');
+const height = document.getElementById('height');
+const weight = document.getElementById('weight');
+
+const ageValue = document.getElementById('ageValue');
+const heightValue = document.getElementById('heightValue');
+const weightValue = document.getElementById('weightValue');
+
+const bmiEl = document.getElementById('bmi');
+const categoryEl = document.getElementById('category');
+const pointer = document.getElementById('pointer');
+
+function calculateBMI(){
+
+    let h = height.value / 100;
+    let w = weight.value;
+
+    let bmi = w / (h*h);
+
+    bmiEl.textContent = bmi.toFixed(1);
+
+    let category = '';
+    let color = '';
+
+    if(bmi < 18.5){
+        category = 'תת משקל';
+        color = '#3b82f6';
+    }
+    else if(bmi < 25){
+        category = 'משקל תקין';
+        color = '#22c55e';
+    }
+    else if(bmi < 30){
+        category = 'עודף משקל';
+        color = '#f59e0b';
+    }
+    else{
+        category = 'השמנת יתר';
+        color = '#ef4444';
+    }
+
+    categoryEl.textContent = category;
+    categoryEl.style.color = color;
+    bmiEl.style.color = color;
+
+    ageValue.textContent = age.value;
+    heightValue.textContent = height.value + ' cm';
+    weightValue.textContent = weight.value + ' kg';
+
+    let position = Math.min(Math.max((bmi / 40) * 100,0),100);
+    pointer.style.left = `calc(${position}% - 60px)`;
+}
+age.addEventListener('input', calculateBMI);
+height.addEventListener('input', calculateBMI);
+weight.addEventListener('input', calculateBMI);
+
+calculateBMI();
